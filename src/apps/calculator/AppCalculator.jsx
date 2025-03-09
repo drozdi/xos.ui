14c3a219dd79ec32ad620bc90fb08127497396c7
Drozdi <drozdi-klim@yandex.ru>
2025-03-09 18:43:30 +0300

    -

--- src/apps/calculator/AppCalculator.jsx
+++ src/apps/calculator/AppCalculator.jsx
@@ -1,52 +1,163 @@
+import { useState } from "react";
 import appsManager from "../../entites/core/apps-manager";
 import { Window } from "../../features";
 import { Box } from "../../shared/internal/box";
 import { XBtn } from "../../shared/ui";
 
 export function AppCalculator() {
+	const [operator, setOperator] = useState("");
+	const [operand1, setOperand1] = useState("");
+	const [operand2, setOperand2] = useState("");
+	const [isResult, setIsResult] = useState(false);
+
+	const onNumClick = (num) => {
+		if (operator) {
+			setOperand2(operand2 ? operand2 + num : String(num || ""));
+		} else {
+			setOperand1(operand1 ? operand1 + num : String(num || ""));
+		}
+	};
+	const onOperatorClick = (operator) => {
+		if (operator === "=") {
+			resultClick();
+		} else if (operator === "C") {
+			clearClick();
+		} else if (operand2) {
+			resultClick();
+			setOperator(operator);
+			setIsResult(false);
+		} else {
+			setOperator(operator);
+			setIsResult(false);
+		}
+	};
+	const clearClick = () => {
+		setOperator("");
+		setOperand1("");
+		setOperand2("");
+		setIsResult(false);
+	};
+	const resultClick = () => {
+		let num1 = +operand1;
+		let num2 = +operand2;
+		setOperand1("");
+		setOperand2("");
+		switch (operator) {
+			case "+":
+				setOperand1(num1 + num2);
+				setOperator("");
+				break;
+			case "-":
+				setOperand1(num1 - num2);
+				setOperator("");
+				break;
+		}
+		setIsResult(true);
+	};
+
 	return (
 		<Window title="Калькулятор">
 			<div className="calculator">
-				<div className="display-screen">
-					<div className="calculation-item">0</div>
-				</div>
+				<Box justify="end">
+					<Box.Header className="text-3xl text-right">0</Box.Header>
+				</Box>
 				<Box col>
 					<XBtn.Group grow pills color="info">
-						<XBtn className="w-1/4">C</XBtn>
-						<XBtn className="w-1/4">A</XBtn>
-						<XBtn className="w-1/4">+/−</XBtn>
-						<XBtn className="w-1/4" color="accent">
+						<XBtn
+							className="w-1/4"
+							onClick={() => onOperatorClick("C")}
+						>
+							C
+						</XBtn>
+						<XBtn
+							className="w-1/4"
+							onClick={() => onOperatorClick("I")}
+						>
+							+−
+						</XBtn>
+						<XBtn
+							className="w-1/4"
+							onClick={() => onOperatorClick("%")}
+						>
+							%
+						</XBtn>
+						<XBtn
+							className="w-1/4"
+							onClick={() => onOperatorClick("+")}
+							color="accent"
+						>
 							÷
 						</XBtn>
 					</XBtn.Group>
-					<XBtn.Group grow pills color="info" className="row">
-						<XBtn className="w-1/4">7</XBtn>
-						<XBtn className="w-1/4">8</XBtn>
-						<XBtn className="w-1/4">9</XBtn>
-						<XBtn className="w-1/4" color="accent">
+					<XBtn.Group grow pills color="info">
+						<XBtn className="w-1/4" onClick={() => onNumClick(7)}>
+							7
+						</XBtn>
+						<XBtn className="w-1/4" onClick={() => onNumClick(8)}>
+							8
+						</XBtn>
+						<XBtn className="w-1/4" onClick={() => onNumClick(9)}>
+							9
+						</XBtn>
+						<XBtn
+							className="w-1/4"
+							onClick={() => onOperatorClick("*")}
+							color="accent"
+						>
 							×
 						</XBtn>
 					</XBtn.Group>
-					<XBtn.Group grow pills color="info" className="row">
-						<XBtn className="w-1/4">4</XBtn>
-						<XBtn className="w-1/4">5</XBtn>
-						<XBtn className="w-1/4">6</XBtn>
-						<XBtn className="w-1/4" color="accent">
+					<XBtn.Group grow pills color="info">
+						<XBtn className="w-1/4" onClick={() => onNumClick(4)}>
+							4
+						</XBtn>
+						<XBtn className="w-1/4" onClick={() => onNumClick(5)}>
+							5
+						</XBtn>
+						<XBtn className="w-1/4" onClick={() => onNumClick(6)}>
+							6
+						</XBtn>
+						<XBtn
+							className="w-1/4"
+							onClick={() => onOperatorClick("-")}
+							color="accent"
+						>
 							−
 						</XBtn>
 					</XBtn.Group>
-					<XBtn.Group grow pills color="info" className="row">
-						<XBtn className="w-1/4">1</XBtn>
-						<XBtn className="w-1/4">2</XBtn>
-						<XBtn className="w-1/4">3</XBtn>
-						<XBtn className="w-1/4" color="accent">
+					<XBtn.Group grow pills color="info">
+						<XBtn className="w-1/4" onClick={() => onNumClick(1)}>
+							1
+						</XBtn>
+						<XBtn className="w-1/4" onClick={() => onNumClick(2)}>
+							2
+						</XBtn>
+						<XBtn className="w-1/4" onClick={() => onNumClick(3)}>
+							3
+						</XBtn>
+						<XBtn
+							className="w-1/4"
+							onClick={() => onOperatorClick("+")}
+							color="accent"
+						>
 							+
 						</XBtn>
 					</XBtn.Group>
-					<XBtn.Group grow pills color="info" className="row">
-						<XBtn className="w-1/2">0</XBtn>
-						<XBtn className="w-1/4">.</XBtn>
-						<XBtn className="w-1/4" color="accent">
+					<XBtn.Group grow pills color="info">
+						<XBtn className="w-1/4" onClick={() => onNumClick(0)}>
+							0
+						</XBtn>
+						<XBtn
+							className="w-1/4"
+							onClick={() => onOperatorClick(".")}
+						>
+							.
+						</XBtn>
+						<XBtn
+							className="w-[calc(50%+6*var(--spacing))]"
+							onClick={() => onOperatorClick("=")}
+							color="accent"
+						>
 							=
 						</XBtn>
 					</XBtn.Group>
